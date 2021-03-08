7f63fcfc0c042075a0c7fc2724d7acb9eee6e02f
Pooja Kukreja <pooja.kukreja@infobeans.com>
2021-03-08 23:21:45 +0530

    temp changes

--- themes/twentytwentyone/index.php
+++ themes/twentytwentyone/index.php
@@ -20,6 +20,7 @@ if ( have_posts() ) {
 
 	// Load posts loop.
 	while ( have_posts() ) {
+		echo "this is for testing";
 		the_post();
 
 		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
